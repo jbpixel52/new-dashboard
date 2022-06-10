50a79172c6138fc3ed2f2d36a0e0875a18033168
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-06-10 11:26:58 -0700

    appicons ts

--- components/AppIcons/AppIcons.tsx
+++ components/AppIcons/AppIcons.tsx
@@ -6,21 +6,19 @@ type App = {
     appdescription: string,
 }
 export default function getIcon(props: App) {
+    const responseurl : String =            "https://raw.githubusercontent.com/walkxhub/dashboard-icons/master/png/" +
+    props.appname.toLowerCase() +
+    ".png"
     if (
-        new Response(
-            "https://raw.githubusercontent.com/walkxhub/dashboard-icons/master/png/" +
-            props.appname.toLowerCase() +
-            ".png"
+        new Response( responseurl.toString()
+ 
         ).status === 200
     ) {
-        return (
-            "https://raw.githubusercontent.com/walkxhub/dashboard-icons/master/png/" +
-            props.appname.toLowerCase() +
-            ".png"
-        );
+        return responseurl;
     }
     if (new Response(props.appurl + "/favicon.ico").status === 200) {
-        return props.appurl.toLowerCase() + "/favicon.ico";
+        const imageurl: String = props.appurl.toLowerCase() + "/favicon.ico"
+        return imageurl;
     }
     return "https://github.com/google/material-design-icons/blob/master/png/alert/error/materialiconsround/48dp/2x/round_error_black_48dp.png";
 
