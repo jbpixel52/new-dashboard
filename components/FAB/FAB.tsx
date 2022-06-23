6c9f6afa7215098a7c8eb943c9a74d8fb7e67962
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-06-22 18:13:03 -0700

    nextjs link instead of compnent event

--- components/FAB/FAB.tsx
+++ components/FAB/FAB.tsx
@@ -5,17 +5,17 @@ import EditIcon from '@mui/icons-material/Edit';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
 import SettingsIcon from '@mui/icons-material/Settings';
+import Link from 'next/link';
 
 
 export default function FloatingActionButtons() {
   return (
     <Box sx={{ '& > :not(style)': { m: 1 } }}>
-      <Fab color="primary" aria-label="add"
-        onClick={() => {
-          window.location = '/settings';
-        }}>
+      <Link href="/settings">
+      <Fab color="primary" aria-label="add">
         <SettingsIcon />
       </Fab>
+      </Link>
       <Fab color="secondary" aria-label="edit">
         <EditIcon />
       </Fab>
